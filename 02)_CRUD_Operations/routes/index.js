4cb1edb682f1d09112a72ea361717534d9f27df6
shilpa511raghav <shilpa511raghav@gmail.com>
2018-11-24 21:10:01 +0530

    crud operatins in json file

--- 02)_CRUD_Operations/routes/index.js
+++ 02)_CRUD_Operations/routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var fs= require("fs");
+const axios = require('axios');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -25,7 +26,7 @@ router.post("/addjson", function(req,res){
     var rollno= req.query.rollno;
     var name= req.query.name;
     if(typeof rollno != 'undefined' & typeof name != 'undefined' ){
-      arr_data.push({rollno,name});
+      arr_data.push({rollno:rollno,name:name});
       var out= JSON.stringify(arr_data);
       fs.writeFileSync(filepath,out);
       res.send(out);
@@ -44,7 +45,7 @@ router.get("/updatejson",function(req,res){
     var rollno= req.query.rollno;
     var name= req.query.name;
     var index= arr_data.findIndex(x => x.rollno== rollno);
-    arr_data[index]= {rollno,name};
+    arr_data[index]= {rollno: rollno,name:name};
     console.log(arr_data);
     fs.writeFileSync(filepath,arr_data);
     res.send(arr_data);
@@ -64,6 +65,30 @@ router.get("/singlerecord", function(req,res){
 
 
 
+//add data
+router.get("/rollno/:rollno/name/:name", function(req,res)
+{
+  fs.readFile(filepath,function(err,data){
+    var ar_data= JSON.parse(data);
+    var qry= req.params;
+    var rollno = qry.rollno;
+    var name= qry.name;
+    ar_data.push({rollno:rollno, name:name});
+    fs.writeFile(filepath,JSON.stringify(ar_data));
+    res.send(ar_data);
+  });
+});
+
+
+//http request to get data from other API
+router.get("/posts",function(req,res){
+  axios.get("https://jsonplaceholder.typicode.com/todos")
+  .then(function(response){ 
+    res.send(response.data);
+  }).catch(function(error){ 
+  });
+  
+});
 
 
 module.exports = router;
